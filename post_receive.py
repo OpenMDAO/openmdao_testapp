e019100d79ff6d44f5a73822118390e6a05a997c
Bret Naylor <naylor.b@gmail.com>
2011-09-06 11:56:34 -0400

    fixed bad call to push_docs

--- post_receive.py
+++ post_receive.py
@@ -159,7 +159,7 @@ def _run_sub(cmd, **kwargs):
 
 
 def push_docs(commit_id):
-    cmd = ['push_docs', '-d', DEVDOCS_DIR]
+    cmd = ['push_docs', '-d', DEVDOCS_DIR, 'http://openmdao.org']
     try:
         out, ret = activate_and_run(os.path.join(LOCAL_REPO_DIR,'devenv'), cmd)
     except Exception as err:
