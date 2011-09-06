93094fb99713f9064c99dd01eab6b1bb408b7b6c
Bret Naylor <naylor.b@gmail.com>
2011-09-06 14:35:09 -0400

    fixed error in db insert

--- model.py
+++ model.py
@@ -64,8 +64,7 @@ def get_docbuild(commit_id):
 
 
 def new_doc_info(commit_id, results):
-    db.insert('docbuilds', commit_id=commit_id, results=results,
-              vars=locals())
+    db.insert('docbuilds', commit_id=commit_id, results=results)
 
     
 def delete_test(commit_id):
