24ee92b68082530cab53de6cceb614abcc64efb8
Bret Naylor <naylor.b@gmail.com>
2011-09-09 10:25:07 -0400

    added a dump function

--- db.py
+++ db.py
@@ -23,16 +23,29 @@ def main():
     cur = conn.cursor()
     cur.execute(cmd)
     cur2 = conn.cursor()
+    
+    tables = []
+    
     print 'TABLES:'
     for n in cur:
-        print n
-        cmd = "SELECT * from %s;" % n
+        print n[0]
+        tables.append(n[0])
+        cmd = "SELECT * from %s;" % n[0]
         cur2.execute(cmd)
         for v in cur2:
             print v
             
     
-    model.dump()
+    for tab in tables:
+        cur = conn.cursor()
+        cur.execute('SELECT * from %s')
+        for result in cur:
+            for r in result:
+                if isinstance(r, basestring):
+                    print r[0:50],
+                else:
+                    print r,
+            print ''
 
 
 
