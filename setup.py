f66529dd1c952f0de395d112f27289249fede726
Bret Naylor <naylor.b@gmail.com>
2012-02-16 16:43:35 -0500

    fixed missing ,

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='openmdao_testapp',
       ],
       entry_points = {
          "console_scripts": [
-             "send_payload = openmdao_testapp.test.test_hook:send_payload"
+             "send_payload = openmdao_testapp.test.test_hook:send_payload",
              "start_server = openmdao_testapp.post_receive:start_server"
          ]
       },
